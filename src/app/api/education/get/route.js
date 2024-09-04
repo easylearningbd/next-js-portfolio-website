f79d185c396585b05972e0fcdbe5c54f634372c9
easylearningbd <elebdsoft@gmail.com>
2024-09-05 02:46:38 +0600

    w14 Create Api for All Components Part 2

--- src/app/api/education/get/route.js
+++ src/app/api/education/get/route.js
@@ -1,5 +1,5 @@
-import connectToDB from "@/database";
-import Home from "@/models/Home";
+import connectToDB from "@/database"; 
+import Education from "@/models/Education";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic"
 export async function GET(req) {
     try {
         await connectToDB();
-        const extractData = await Home.find({}); 
+        const extractData = await Education.find({}); 
 
         if (extractData) {
             return NextResponse.json({
--- src/app/api/experince/get/route.js
+++ src/app/api/experince/get/route.js
@@ -1,5 +1,5 @@
-import connectToDB from "@/database";
-import Home from "@/models/Home";
+import connectToDB from "@/database";  
+import Experince from "@/models/Experince";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic"
 export async function GET(req) {
     try {
         await connectToDB();
-        const extractData = await Home.find({}); 
+        const extractData = await Experince.find({}); 
 
         if (extractData) {
             return NextResponse.json({
