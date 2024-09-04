060233c25645d2749f9e7dcdfce4e70ed63643b2
easylearningbd <elebdsoft@gmail.com>
2024-09-04 14:00:33 +0600

    w07 Create Home Form for Store Data Part 2

--- src/components/admin-view/home/index.jsx
+++ src/components/admin-view/home/index.jsx
@@ -1,5 +1,7 @@
 'use client'
 
+import FormControls from "../form-controls"
+
 const controls = [
     {
         name: 'heading',
@@ -16,9 +18,15 @@ const controls = [
 ] 
  
 export default function AdminHomeView({formData,setFormData}){
+    console.log(formData);
     return <div className="w-full">
             <div className="bg-[#d7d7d7] shadow-md rounded px-8 pt-6 pb-8 mb-4">
         
+        <FormControls controls={controls}
+         formData={formData}
+         setFormData={setFormData}
+        /> 
+
         <button className="mt-[5px] border border-blue-600 bg-blue-600 text-white p-3 font-bold text-[16px] focus:bg-green-800 rounded-md">
             Add Info
         </button>
