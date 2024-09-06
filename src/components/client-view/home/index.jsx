f07de647f9356629d8dfb9e48f2d81acb2268af2
easylearningbd <elebdsoft@gmail.com>
2024-09-07 03:19:26 +0600

    w35 Design Frontend Home Page Part 1

--- src/components/client-view/home/index.jsx
+++ src/components/client-view/home/index.jsx
@@ -1,5 +1,23 @@
 'use client'
+
+import AnimationWrapper from "../animation-wrapper";
+import { motion } from 'framer-motion';
+
+function variants(){
+    
+}
+
 export default function ClientHomeView({data}){
     console.log(data,'ClientHomeView');
-    return <div>Client Home View</div>
+    return (
+        <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="home">
+        <AnimationWrapper>
+            <motion.div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-12 py-10 sm:py-20" variants={setVariants}>
+
+            </motion.div>
+
+        </AnimationWrapper>
+
+        </div>
+    ) 
 }
