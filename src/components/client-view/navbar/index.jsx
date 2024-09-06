abb640a0ec6bddee131342c33761cbce544b905c
easylearningbd <elebdsoft@gmail.com>
2024-09-07 01:08:02 +0600

    w32 Design Frontend NavBar Part 2

--- src/components/client-view/navbar/index.jsx
+++ src/components/client-view/navbar/index.jsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useState } from "react";
+import Image from "next/image";
+import logo from "../../../assets/logo.png"
+import { Link as LinkScroll, scroller } from "react-scroll";
 
 const menuItems = [
     {
@@ -25,6 +28,26 @@ const menuItems = [
     },
 ];
 
+function CreateMenus({ activeLink,getMenuItems, setActiveLink }) {
+    return getMenuItems.map((item) => (
+        <LinkScroll
+        activeClass="active"
+        to={item.id}
+        spy={true}
+        smooth={true}
+        duration={1000}
+        onSetActive={() => setActiveLink(item.id)}
+        className={`px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative ${
+            activeLink === item.id 
+            ? "text-green-main animation-active shadow-green-main"
+            : "text-[#000] font-bold hover:text-green-main"
+        } `}
+        >
+            {item.label}
+        </LinkScroll>
+    ))
+}
+
 export default function Navbar(){
 
     const [activeLink, setActiveLink] = useState("home");
@@ -39,9 +62,26 @@ export default function Navbar(){
         <div className="col-start-1 col-end-2 flex items-center">
             <div className="cursor-pointer flex gap-2 font-bold items-center text-[20px] text-green-main">
 
-            </div>
+        <Image
+        src={logo}
+        alt="Logo"
+        layout="responsive"
+        quality={100}
+        height={100}
+        width={120}
+        />
 
-        </div>
+            </div> 
+        </div> 
+    <ul className="hidden lg:flex col-start-4 col-end-8 text-[#000] items-center">
+        <CreateMenus
+        setActiveLink={setActiveLink}
+        activeLink={activeLink}
+        getMenuItems={menuItems}
+        />
+
+    </ul>
+
 
     </nav>
 
