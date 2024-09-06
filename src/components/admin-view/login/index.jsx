ec14058390a87f7a20b1081cecd3df65db71979d
easylearningbd <elebdsoft@gmail.com>
2024-09-06 14:27:35 +0600

    w25 Login Authentication Part 2

--- src/components/admin-view/login/index.jsx
+++ src/components/admin-view/login/index.jsx
@@ -19,15 +19,15 @@ const controls = [
   
 export default function Login({formData,setFormData,handleSaveData}){
     // console.log(formData);
-    return <div className="w-full">
-            <div className="bg-[#d7d7d7] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    return <div className="min-h-screen flex items-center justify-center ">
+            <div className="w-full max-w-md bg-slate-400 shadow-lg rounded-lg px-8 pt-6 pb-8">
         
         <FormControls controls={controls}
          formData={formData}
          setFormData={setFormData}
         /> 
 
-        <button onClick={() => handleSaveData('home')} className="mt-[5px] border border-blue-600 bg-blue-600 text-white p-3 font-bold text-[16px] focus:bg-green-800 rounded-md">
+        <button onClick={() => handleSaveData('home')} className="w-full mt-6 bg-blue-600 text-white font-bold py-3 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition duration-300">
             Login
         </button>
 
