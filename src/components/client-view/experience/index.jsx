2e0659310eebc7de7d96fe1141f9525240d8baf9
easylearningbd <elebdsoft@gmail.com>
2024-09-08 01:31:42 +0600

    w42 Design Frontend Experience and Education Page Part 1

--- src/components/client-view/experience/index.jsx
+++ src/components/client-view/experience/index.jsx
@@ -1,4 +1,30 @@
 'use client'
-export default function ClientExperienceAndEducationView(){
-    return <div> Client Experience Education View</div>
+import { Timeline,TimelineConnector,TimelineContent, TimelineDot,TimelineItem,TimelineSeparator } from "@mui/lab";
+import AnimationWrapper from "../animation-wrapper";
+import { motion } from "framer-motion";
+
+export default function ClientExperienceAndEducationView({educationData,experienceData}){
+    // console.log(educationData,experienceData,"educationData");
+
+    return (
+    <div className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto" id="experience">
+
+        <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8">
+            <div className="flex flex-col gap-5">
+        <AnimationWrapper className={"py-6 sm:py-16"}>
+            <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+        <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-bold">
+            {"My Experince".split(" ").map((item,index) => (
+                <span className={`${index === 1 ? "text-green-main" : "text-[#000]"}`}> {item}{" "} </span>
+            ))}
+        </h1>
+            </div>
+
+        </AnimationWrapper>
+            </div>
+
+        </div>
+
+    </div>
+    )
 }
